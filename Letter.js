d2ff289721bccb767f5594ab074b6a67267e8d81
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-05-12 17:20:04 -0500

    Letter.js is working

--- Letter.js
+++ Letter.js
@@ -1,42 +1,34 @@
-var Letter = function (letter) {
-  this.letter = letter;
-  this.guessed = false;
-  // ? A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-  this.guessLetter = function (letterGuess) {
-    this.guess = letterGuess;
-    if (this.guess === this.letter) {
-      console.log('the guess is: \t' + this.guess);
-      console.log('you guessed correctly\n');
-      this.guessed = true;
-    } else {
-      console.log('the guess is: \t' + this.guess);
-      console.log('you are incorrect\n');
-      this.guessed = false;
-    }
-    this.showLetter();
-  };
+ // * letter Constructor
+ var Letter = function (letter) {
+   this.letter = letter;
+   this.isGuessed = false;
 
+   // ? this function takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+   this.guessLetter = function (letterGuess) {
+     this.guess = letterGuess;
+     if (this.guess === this.letter) {
+       console.log('\t\tthe guess is: \t' + this.guess);
+       console.log('\t\tyou guessed correctly\n');
+       this.isGuessed = true;
+     } else {
+       console.log('\t\tthe guess is: \t' + this.guess);
+       console.log('\t\tyou are incorrect\n');
+       this.isGuessed = false;
+     }
+     this.showLetter();
+   };
 
-  // ? A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-  this.showLetter = function () {
-    if (this.guessed === true) {
-      console.log(this.letter);
-    } else {
-      console.log('_');
-    }
-  };
+   // ? this function returns the character if the letter has been guessed, or an underscore if the letter has not been guessed
+   this.showLetter = function () {
+     if (this.isGuessed === true) {
+       console.log("\t\t\t", this.letter, "\n");
+     } else {
+       console.log('\t\t\t_\n');
+     }
+   };
+ }
 
-}
+ var letterB = new Letter('b');
 
-
-
-// var letterA = new Letter('a');
-var letterB = new Letter('b');
-// var letterC = new Letter('c');
-// letterA.showLetter();
-// letterB.showLetter('g');
-letterB.guessLetter('g');
-
-// letterB.showLetter('b');
-letterB.guessLetter('b');
-// letterB.showLetter('b');
+ letterB.guessLetter('g');
+ letterB.guessLetter('b');
