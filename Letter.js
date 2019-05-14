4636bdc3ce657450da4e052aca6b0bac0c01051d
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-05-14 09:25:52 -0500

    toString function working

--- Letter.js
+++ Letter.js
@@ -3,33 +3,53 @@
    this.letter = letter;
    this.isGuessed = false;
 
+
+   //  ? going to use this to make the space a * or other character to help identify
+   //  console.log(this.letter);
+   //  if (this.letter === ' ') {
+   //    //  console.log('a space ', this.letter);
+   //    this.newLetter = this.letter.replace(/' '/g, "*");
+   //    //  console.log('new Letter', this.newLetter);
+   //    //  console.log(this.letter);
+   //  }
+
    // ? this function takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
    this.guessLetter = function (guess) {
      this.guess = guess;
+     console.log('\t\tthe guess is: \t"' + this.guess + '"\n');
+
      if (this.guess === this.letter) {
-       //  console.log('\t\tthe guess is: \t' + this.guess);
-       console.log('\t\tyou are correct');
+       console.log('\t\tyou are correct\n');
        this.isGuessed = true;
      } else {
-       //  console.log('\t\tthe guess is: \t' + this.guess);
-       console.log('\t\tyou are incorrect');
+       console.log('\t\tyou are incorrect\n');
        this.isGuessed = false;
      }
-     this.showLetter();
+     //  this.showLetter();
    };
 
    // ? this function returns the character if the letter has been guessed, or an underscore if the letter has not been guessed
    this.showLetter = function () {
+
      if (this.isGuessed === true) {
        console.log("\t\t\t", this.letter, "\n");
+       //  this.letter = this.letter;
+       //  return this.letter;
      } else {
        console.log('\t\t\t_\n');
+       //  this.letter = '-';
+       //  return this.letter;
      }
    };
  }
 
  var letterB = new Letter('b');
+
+ console.log("The letter is " + letterB.letter);
+
  letterB.guessLetter('g');
- //  letterB.guessLetter('b');
+ letterB.showLetter('a');
+ letterB.guessLetter('b');
+
 
  module.exports = Letter;
--- Words.js
+++ Words.js
@@ -2,50 +2,78 @@ var Letter = require('./Letter');
 
 
 var Word = function (word) {
-
+  this.word = word;
+  console.log('this is the word:', this.word);
   this.word = word.split('');
-  console.log(this.word, '\n');
+  console.log('this.word after split: ', this.word);
+
   this.letterArray = [];
+
+  this.newWord = [];
+
   for (var i = 0; i < this.word.length; i++) {
-
-    // this.i = new Letter(this.word[i]);
     this.letterArray.push(new Letter(this.word[i]));
-    // console.log(this.letter[i]);
-    // console.log(this.word[i]);
-    // console.log(this.i);
   }
-  console.log(this.letterArray);
+
+
+
+  // console.log("\n\t\t\tThis is the current letterArray", this.letterArray, "\t\t\t\t");
 }
-
-
-
 
 // ? A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-Word.prototype.toString = function () {
-  console.log('\nthe string looks like:', this.word.join('').toString(), '\n');
+Word.prototype.toString = function (guess) {
+
+  !console.log('\n\tstring looks like this before:', this.word.join('', '\n'));
+
 
   for (var j = 0; j < this.word.length; j++) {
-    this.letterArray[j].guessLetter('t');
-    // this.word[j].guessLetter();
-    // console.log(this.word[j]);
+    this.newWord.push(this.letterArray[j].guessLetter(guess));
+    this.newWord.push(this.letterArray[j].letter);
   }
-  // for (i = 0; i < this.word.letter.length; i++) {
-  //   //     console.log(this.word.letter[i])
-  //   //     // this.word.letter[i].guessLetter('c');
 
-  // }
-
+  this.newWord = this.newWord.join('').toString();
+  console.log('\n\tNEW string after guessing "' + guess + '": ' + this.newWord);
+  console.log('\n');
 }
 
+
+
 // ? A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+Word.prototype.wordGuess = function (letter) {
+  console.log('this is the word:', this.word.join(''));
+  // console.log(this.letterArray);
+
+  for (var k = 0; k < this.word.length; k++) {
+    // console.log(this.word[k]);
+    console.log(this.letterArray[k]);
+    console.log(this.letterArray[k].showLetter());
+
+
+    // console.log(this.letterArray[k].showLetter(letter));
+    // console.log(this.letterArray[k].length);
+    console.log(this.letterArray[k].letter);
+    // console.log(this.letterArray[k].letter.showLetter());
+    // this.newWord.push(this.letterArray[k].showLetter(letter));
+    // this.newWord.push(this.letterArray[k].letter);
+
+  }
+  console.log('\n\tNEW string after guessing: "' + letter + '": ' + this.newWord.join('').toString());
+
+
+}
 
 
 
 var cat = new Word('the cat');
 
-cat.toString();
+// cat.toString(' ');
+// cat.toString(' ');
+
+cat.wordGuess('c')
 
 var letterC = new Letter('c');
+
+// console.log(letterC);
 
 // letterC.guessLetter('x');
 
